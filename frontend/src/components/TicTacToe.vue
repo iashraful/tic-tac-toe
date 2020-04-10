71d7e09c72cc4993f109d2b5e3ff2d5f1e14b12f
Ashraful Islam <ashrafulrobin3@gmail.com>
2020-04-10 10:17:02 +0600

    Adding frontend for tictac toe

---
+++ frontend/src/components/TicTacToe.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tictactoe-board">
+    <div v-for="(n, i) in 3" :key="i">
+      <div v-for="(n, j) in 3" :key="j">
+        <cell :value="board[i][j]"></cell>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from './Cell'
+
+export default {
+  name: 'TicTacToe',
+  components: { Cell },
+  data () {
+    return {
+      board: [
+        ['', '', ''],
+        ['', 'x', ''],
+        ['', '', '']
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    width: 204px;
+    height: 204px;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <tic-tac-toe/>
+  </div>
+</template>
+
+<script>
+import TicTacToe from '../components/TicTacToe'
+
+export default {
+  name: 'Home',
+  components: { TicTacToe }
+}
+</script>
+
+<style>
+.home {
+  display: flex;
+  justify-content: center;
+}
+</style>
